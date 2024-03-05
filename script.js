eaf58ed2593a6b1d2cff709d0133378ec466c5d7
TharunReddyCSE <tharunreddy14march@gmail.com>
2024-03-05 19:48:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,25 +13,3 @@ for (i = 0; i < acc.length; i++) {
         }
     });
 }
-
-// var arr = document.getElementsByClassName("arr");
-// var j;
-
-// for (j = 0; j < acc.length; j++) {
-//     acc[j].addEventListener("click", function () {
-//         // this.classList.toggle("active");
-//         arr.classList.toggle("rotate");
-//     });
-// }
-
-
-// var accordions = document.querySelectorAll(".accordion");
-// var arr = document.querySelectorAll(".arr");
-
-// function toggleRot() {
-//     // arr.classList.toggle("rotate")
-//     arr.forEach(ar => ar.classList.toggle("rotate"));
-//     console.log(this);
-// }
-
-// accordions.forEach(accordion => accordion.addEventListener('click', toggleRot));
